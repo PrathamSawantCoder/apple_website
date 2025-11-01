16d8d569a40da739e9f5cb0cd8f5641f8ec149ca
PrathamSawantCoder <prathamnsawant@gmail.com>
2025-11-01 12:19:03 +0200

    Implemented 3D Virtual Product Viewer Section with options for colors and scale.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar.tsx"
 import Hero from "./components/Hero.tsx"
+import ProductViewer from "./components/ProductViewer.tsx"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <Navbar/>
-      ´<Hero/>
+      <Hero/>
+      <ProductViewer/>
     
     </main>
     
