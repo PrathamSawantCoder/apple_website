1e7c85eb9e17dcababa8b9e258e01a9f0301cf4d
PrathamSawantCoder <prathamnsawant@gmail.com>
2026-02-12 18:31:50 +0200

    Refactor Footer component to include shopping options and copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
-
+import { footerLinks } from "../constants/index";
 
 const Footer = () => {
   return (
-    <section>
-        
-    </section>
+    <footer>
+      <div>
+        <p>More ways to shop: <a className="text-blue">Find an Apple Store</a> or <a className="text-blue">other retailer</a> near you. Or call 1-800-MY-APPLE.</p>
+        <img src="/logo.svg" alt="Apple Logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({label, link}) =>(
+            <li key={label} >
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
